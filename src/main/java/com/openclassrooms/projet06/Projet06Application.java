5fc28d545b7cf5af65590a218255a499b7e39ebf
Visalinthan <cvisalinthan@yahoo.fr>
2021-12-31 17:21:23 +0100

    init project with dependencies

---
+++ src/main/java/com/openclassrooms/projet06/Projet06Application.java
@@ -0,0 +1,13 @@
+package com.openclassrooms.projet06;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Projet06Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Projet06Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/openclassrooms/projet06/Projet06ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.openclassrooms.projet06;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Projet06ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
